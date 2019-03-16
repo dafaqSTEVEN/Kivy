d296a70f8f2518a646d3a93b7a3471b5a1bed839
dafaqSTEVEN <36439494+dafaqSTEVEN@users.noreply.github.com>
2019-03-16 17:59:40 +0800

    database 0.0.3

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
-
+import os
+os.environ['KIVY_IMAGE'] = 'sdl2'
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
@@ -7,19 +8,25 @@ from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.config import Config
+from kivy.graphics import Color, Rectangle,Line
+from kivy.core.image import Image 
+from kivy.uix.image import Image
 
-Config.set('graphics', 'height', '800')
-Config.set('graphics', 'width', '700')
+Config.set('graphics', 'height', '1920')
+Config.set('graphics', 'width', '1080')
 
+
+exec(open(r'E:\Program\GITHUBKIVY\MY\Kivy\time.py').read())
 
 class App(App):
 	def build(self):
 		def login(self):
-				if t1.text == '123' and t2.text == '123':
+				if t1.text == 'admin' and t2.text == 'admin':
 					t1.text = ''
 					t2.text = ''
 					l3.text =  'success'
@@ -29,46 +36,47 @@ class App(App):
 					l3.text = 'invalid'
 		def back(self):
 			s.current = 'screen1'
+		root = FloatLayout(size = (1080,1920))
+		with root.canvas.before:
+			Color(255, 255, 255, 0.7)
+			self.rect = Rectangle(size = root.size)
+			
 		s = ScreenManager()
 		screen1 = Screen(name = 'screen1')
+		with screen1.canvas.before:
+			Line(points=[100, 700, 400, 700], width=3)
+			Line(points=[600, 700, 900, 700], width=3)
 		screen2 = Screen(name = 'screen2')
-		a = AnchorLayout(anchor_x = 'left',anchor_y = 'center')
-		g = BoxLayout(size_hint=(None, None),orientation = 'vertical',padding = [0,10,50,10])
-		b1 = BoxLayout(size_hint=(None, None))
-		b2 = BoxLayout(size_hint=(None, None))
-		b3 = BoxLayout(size_hint=(None, None))
-		l1 = Label(text = 'Username:',font_size = 30,size_hint = (None,None),size = (200,100))
-		t1 = TextInput(font_size = 30,size_hint = (None,None),size = (200,50))
-		l2 = Label(text = 'Password:',font_size = 30,size_hint = (None,None),size = (200,100))
-		t2 = TextInput(font_size = 30 ,size_hint = (None,None),size = (200,50))
-		btn = Button(text = 'Login' ,size_hint = (None,None),size = (200,50),font_size = 30)
-		l3 = Label(text = 'await' ,font_size = 30,size_hint = (None,None),size = (200,50))
+		g = BoxLayout(pos =(190,800),size_hint=(700,700),orientation = 'vertical')
+		logo = Label(font_name = 'FONT',markup = True,text = '[color=000000]ASSIST[/color]' ,size_hint = (None,None),size = (300,90),font_size = 200,pos = (350,1400))
+		t1 = TextInput(hint_text = 'Username',font_size = 20,size_hint = (None,None),size = (600,50))
+		space = Label(size_hint = (None,None),size = (400,50))
+		t2 = TextInput(hint_text = 'Password',font_size = 20 ,size_hint = (None,None),size = (600,50),password = True)
+		btn = Button(font_name = 'FONT',markup = True,text = '[color=FFFFFF]LOGIN[/color]' ,size_hint = (None,None),size = (100,30),font_size = 20,pos = (450,680))
+		l3 = Label(text = 'await') ,font_size = 30,size_hint = (None,None),size = (200,50),markup = True,pos = (50,50))
 		btn.bind(on_press = login)
-		t4 = Label(text = 'Done' ,font_size = 30,size_hint = (None,None),size = (200,100))
-
-		b4 = BoxLayout(size_hint=(None, None))
-		btn2 = Button(text = 'Back',font_size = 30,size_hint = (None,None),size = (200,50))
+		t4 = Label(text = 'SIGNED IN' ,font_size = 30,size_hint = (None,None),size = (200,100))
+		b4 = BoxLayout(pos_hint={"x":0.5,"y":0.7},size_hint=(None, None))
+		btn2 = Button(text = 'Log Out',font_size = 30,size_hint = (None,None),size = (200,50))
 		btn2.bind(on_press = back)
+		root.add_widget(s)
 		s.add_widget(screen1)
 		s.add_widget(screen2)
 		screen2.add_widget(b4)
 		b4.add_widget(t4)
 		b4.add_widget(btn2)
 		s.current = 'screen1'
-		screen1.add_widget(a)
-		a.add_widget(g)
-		g.add_widget(b1)
-		g.add_widget(b2)
-		g.add_widget(b3)
-		b1.add_widget(l1)
-		b1.add_widget(t1)
-		b2.add_widget(l2)
-		b2.add_widget(t2)
-		b3.add_widget(btn)
-		b3.add_widget(l3)
+		screen1.add_widget(g)
+		screen1.add_widget(l3)
+		screen1.add_widget(logo)
+		screen1.add_widget(btn)
+		g.add_widget(t1)
+		g.add_widget(space)
+		g.add_widget(t2)
+
 		
 		
-		return s
+		return root
 		
 
 if __name__ == '__main__':
---
+++ time.py
@@ -0,0 +1,65 @@
+import csv
+
+agency = {}
+
+agency_id = []
+agency_name = []
+agency_url = []
+agency_timezone = []
+agency_lang = []
+agency_phone = []
+agency_email = []
+
+
+with open("E:\\Program\\GITHUBKIVY\\MY\\Kivy\\bus_en\\agency.csv") as f:
+    data = csv.DictReader(f, delimiter=',')
+    for row in data:
+        temp_agency_id = row['agency_id']
+        agency_id.append(temp_agency_id)
+        temp_agency_name = row['agency_name']
+        agency_name.append(temp_agency_name)
+        temp_agency_url = row['agency_url']
+        agency_url.append(temp_agency_url)
+        temp_agency_timezone = row['agency_timezone']
+        agency_timezone.append(temp_agency_timezone)
+        temp_agency_lang = row['agency_lang']
+        agency_lang.append(temp_agency_lang)
+        temp_agency_phone = row['agency_phone']
+        agency_phone.append(temp_agency_phone)
+        temp_agency_email = row['agency_email']
+        agency_email.append(temp_agency_email)
+
+'''
+print(agency_id)
+print('------------------------------------------')
+print(agency_name)
+print('------------------------------------------')
+print(agency_url)
+print('------------------------------------------')
+print(agency_timezone)
+print('------------------------------------------')
+print(agency_lang)
+print('------------------------------------------')
+print(agency_phone)
+print('------------------------------------------')
+print(agency_email)
+'''
+
+for i in range(len(agency_id)):
+    agency[str(agency_id[i])] = [agency_id[i],agency_name[i],agency_url[i],agency_timezone[i],agency_lang[i],agency_phone[i],agency_email[i]]
+'''
+g = input('Tell me the agency ID : ')
+
+print(agency[g.upper()])
+'''
+'''
+h = input('Tell me the Agency ID : ')
+j = input('Tell me the info :')
+if j =='agency_id':
+    x = 0
+elif j =='agency_name':
+    x = 1
+temp = agency[h.upper()]
+print(temp[x])
+'''
+
